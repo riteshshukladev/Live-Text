264f479845b39dc3e730a05f560587ac17c23ed4
RItesh <shuklaritesh5678@gmail.com>
2024-03-12 23:03:14 +0530

    code

--- server/knexfile.js
+++ server/knexfile.js
@@ -17,20 +17,20 @@ const CertificatePath = path.join(__dirname,'ssl','ca-certificate.crt');
 
 
 export default {
-  // development: {
-  //   client: 'pg',
-  //   connection: {
-  //     database: process.env.DB_DATABASE,
-  //     user: process.env.DB_USER,
-  //     password: process.env.DB_PASSWORD,
-  //     host: process.env.DB_HOST,
-  //     port: process.env.DB_PORT,
-  //     // ssl:{
-  //     //   rejectUnauthorized:true,
-  //     //   ca:fs.readFileSync(CertificatePath).toString(),
-  //     // },
-  //   }
-  // },
+  development: {
+    client: 'pg',
+    connection: {
+      database: process.env.DB_DATABASE,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      ssl:{
+        rejectUnauthorized:true,
+        ca:fs.readFileSync(CertificatePath).toString(),
+      },
+    }
+  },
  
 
   production: {
